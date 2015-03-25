0f0f15569a432676377fe31780751f37a6cb5b1d
jonschlinkert <jon.schlinkert@sellside.com>
2015-03-24 22:10:24 -0400

    lint

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 /*!
  * has-any-deep <https://github.com/jonschlinkert/has-any-deep>
  *
- * Copyright (c) 2014 Jon Schlinkert, contributors.
- * Licensed under the MIT License
+ * Copyright (c) 2014-2015, Jon Schlinkert.
+ * Licensed under the MIT License.
  */
 
 'use strict';
